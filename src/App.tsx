2d4d3887ee803af5875b7f20e0f1c02fd10a0ca0
Sol Martinez <solcito.marti@hotmail.com>
2023-05-03 19:28:09 -0300

    Avanzo con íconos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import Quote from './assets/wired-lineal-41-quotation-mark-second.gif';
+import EditDocument from './assets/wired-lineal-245-edit-document.gif';
+import Walk from './assets/wired-lineal-646-walking-walkcycle-person.gif';
+import Pencil from './assets/wired-lineal-35-edit.gif';
+import Clock from './assets/wired-lineal-236-alarm-clock.gif';
+import Gift from './assets/wired-lineal-412-gift.gif';
 import './App.css';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
@@ -6,43 +12,82 @@ type FormElement = React.FormEvent<HTMLFormElement>;
 interface IPath {
   title: string;
 };
+interface IDeadline {
+  date: string;
+};
 
 function App(): JSX.Element {
   const [newPath, setNewPath] = useState<string>('');
   // const [paths, setPaths] = useState<IPath[]>([]);
+
   const [listOfPaths, setListOfPaths] = useState<IPath[]>([
     {
-      title: 'El camino más rápido'
+      title: 'El camino más rápido.'
     },
     {
-      title: 'El camino más completo'
+      title: 'El camino más completo.'
     },
     {
-      title: 'El camino de menos esfuerzo'
+      title: 'El camino de menos esfuerzo.'
     },
     {
-      title: 'El camino hacia el reconocimiento'
+      title: 'El camino hacia el reconocimiento.'
     },
     {
-      title: 'El camino esperable'
+      title: 'El camino esperable.'
     },
     {
-      title: 'El camino por el que se inclina más tu corazón'
+      title: 'El camino por el que se inclina más tu corazón.'
     },
     {
-      title: 'El camino más polémico'
+      title: 'El camino más polémico.'
     },
     {
-      title: 'El camino más tradicional'
+      title: 'El camino más tradicional.'
     },
     {
-      title: 'El camino más innovador'
+      title: 'El camino más innovador.'
     },
   ]);
   const [showPath, setShowPath] = useState<boolean>(false);
   const [pathToShow, setPathToShow] = useState<IPath>({
     title: ''
   });
+  const [showDeadline, setShowDeadline] = useState<boolean>(false);
+  const [listOfDeadlines, setListOfDeadlines] = useState<IDeadline[]>([
+    {
+      date: "1 semana"
+    },
+    {
+      date: "10 días"
+    },
+    {
+      date: "2 semanas"
+    },
+    {
+      date: "1 mes"
+    },
+    {
+      date: "2 meses"
+    },
+    {
+      date: "3 días"
+    },
+    {
+      date: "1 día"
+    },
+    {
+      date: "6 horas"
+    },
+    {
+      date: "3 semanas"
+    }
+  ]);
+  const [deadlineToShow, setDeadlineToShow] =  useState<IDeadline>({
+    date: ''
+  });
+
+  const [visible, setVisible] = useState<boolean>(false);
 
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
@@ -61,9 +106,10 @@ function App(): JSX.Element {
 
   const handleSeePaths = () => {
     const length = listOfPaths.length;
+    
+    const id = Math.floor(Math.random() * length);
 
-    // numero random entre la cantidad de length
-    // uso ese numero como index y seteo ese objecto como pathToShow
+    setPathToShow(listOfPaths[id]);
     setShowPath(true);
   };
 
@@ -71,11 +117,31 @@ function App(): JSX.Element {
     setShowPath(false);
   };
 
+  const handleSeeDeadline = () => {
+    const length = listOfDeadlines.length;
+    
+    const id = Math.floor(Math.random() * length);
+
+    setDeadlineToShow(listOfDeadlines[id]);
+    setShowDeadline(true);
+  };
+
+  const handleSurprise = () => {
+    setVisible(true);
+  }
+
   return (
     <div className="paper">
     <div className="pattern">
       <div className="content">
 
+      {/* SORPRESA */}
+      <div className="div-surprise" onClick={handleSurprise}>
+        <img src={Gift} alt="gift" className="img-surprise" />
+      </div>
+      {/* DIV ABSOLUTE */}
+
+      <img src={EditDocument} alt="edit-document" className="img-document" />
       <div className="typewriter-div">
         <div className="typewriter">
           Herramienta anti-procrastinación:
@@ -85,8 +151,10 @@ function App(): JSX.Element {
 
     {/* BOTÓN PARA ELEGIR UN CAMINO */}
     <div>
-      <p className="paragraph">¿Qué camino voy a seguir para encarar este proyecto?</p>
-      
+      <div className="div-showPath">
+        <img src={Walk} alt="walk" className="img-walk" />
+        <p className="paragraph">¿Qué camino voy a seguir para encarar este proyecto?</p>
+      </div>
       <br/>
       <div className="button-1div">
         <button onClick={handleSeePaths} className="button-1">
@@ -109,17 +177,24 @@ function App(): JSX.Element {
       <br/>
       <div>
         {showPath ? (
-          <h3>{pathToShow.title}</h3>
+          <div className="div-showPath">
+            <img src={Quote} alt="quotes" className="img-quotes" />
+
+            <h3 className='to-show'>{pathToShow.title}</h3>
+
+          </div>
         ): null}
-        {/* MOSTRAR CAMINO */}
       </div>
     </div>
 
     {/* CAMINO PERSONALIZADO */}
     <div>
-      <p className="paragraph">
-        Agregá un camino extra:
-      </p>
+      <div className="div-showPath">
+        <img src={Pencil} alt="pencil" className="img-walk" />
+        <p className="paragraph">
+          Agregá un camino extra:
+        </p>
+      </div>
 
       <br/>
       <form onSubmit={handleSubmit} className="form">
@@ -139,24 +214,22 @@ function App(): JSX.Element {
 
     {/* DEADLINE */}
     <div>
-      <p className="paragraph">¿Necesitás un plazo de días para meterte presión?</p>
-      <p className="paragraph">Apretá el botón recibir para una fecha aleatoria</p>
+      <div className="div-showPath">
+        <img src={Clock} alt="clock" className="img-clock" />
+        <div>
+          <p className="paragraph-two">¿Necesitás un plazo de días para meterte presión?</p>
+          <p className="paragraph-two">Apretá el botón recibir para una fecha aleatoria</p>
+        </div>
+      </div>
+
+      <button onClick={handleSeeDeadline}>Deadline</button>
+
+      {showDeadline ? (
+        <div className="div-showPath">
+          <h4 className="to-show">{deadlineToShow.date}</h4>
+        </div>
+      ): null}
     </div>
-
-
-    {/*
-      EXTRA MOTIVACIÓN: Un botón que haga aparecer un modal una foto
-    */}
-
-    {/* -------- ESTO NO VA A APARECER ------------ */}
-    {
-      // listOfPaths.map((t: IPath, i: number) =>{
-      //   return <h1 key={i}>
-      //     {t.title}
-      //   </h1>
-      // })
-    }
-    {/* ------------------------------------------- */}
         </div>
       </div >
     </div >
